6c082a4ffdb083f598c96ebde0670723080d628f
NatanaelRomario <88253074+NatanaelRomario@users.noreply.github.com>
2024-03-20 20:47:07 -0300

    Add files via upload

---
+++ Fundamentos/inicio/desafio.js
@@ -0,0 +1,8 @@
+// PI = 
+// PI * RAIO * RAIO
+const PI = 3.141592
+let RAIO = 10;
+let areaCirc = PI * RAIO * RAIO;
+
+console. log("O valor da área é " + areaCirc + "m2.");
+
---
+++ Fundamentos/inicio/fundamentos/nullunderfined.js
@@ -0,0 +1,6 @@
+let valor
+console.log(valor)
+console.log(valor2)
+
+
+
